a0b66b587feb3788236e20f5051704cb86c80441
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-27 23:32:00 -0500

    Se agrega los cambios realizados en la vista de producto

--- programacion3/SubastaQuindio/src/main/java/co/edu/uniquindio/programacion3/subastaquindio/viewController/ProductoViewController.java
+++ programacion3/SubastaQuindio/src/main/java/co/edu/uniquindio/programacion3/subastaquindio/viewController/ProductoViewController.java
@@ -1,4 +1,52 @@
 package co.edu.uniquindio.programacion3.subastaquindio.viewController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
+
 public class ProductoViewController {
+
+    @FXML
+    private Rectangle ImageView;
+
+    @FXML
+    private ComboBox<?> cmbTipoProducto;
+
+    @FXML
+    private DatePicker dateFechaFinPublicacion;
+
+    @FXML
+    private DatePicker dateFechaPublicación;
+
+    @FXML
+    private TextArea txaDescripción;
+
+    @FXML
+    private TextField txfAnunciante;
+
+    @FXML
+    private TextField txfNombreProducto;
+
+    @FXML
+    private TextField txfValorInicial;
+
+    @FXML
+    void actualizarProducto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarProducto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarProducto(ActionEvent event) {
+
+    }
+
 }
