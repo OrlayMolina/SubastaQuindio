a0d354c942927b751c9b9ea87cb15b124fd455bb
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-29 20:15:59 -0500

    Se actualiza el proyecto

--- programacion3/SubastaQuindio/src/main/java/co/edu/uniquindio/programacion3/subastaquindio/controller/ModelFactoryController.java
+++ programacion3/SubastaQuindio/src/main/java/co/edu/uniquindio/programacion3/subastaquindio/controller/ModelFactoryController.java
@@ -1,4 +1,66 @@
 package co.edu.uniquindio.programacion3.subastaquindio.controller;
 
-public class ModelFactoryController {
+import co.edu.uniquindio.programacion3.subastaquindio.controller.service.IModelFactoryService;
+import co.edu.uniquindio.programacion3.subastaquindio.exceptions.ProductoException;
+import co.edu.uniquindio.programacion3.subastaquindio.mapping.dto.ProductoDTO;
+import co.edu.uniquindio.programacion3.subastaquindio.mapping.mappers.SubastaMapper;
+import co.edu.uniquindio.programacion3.subastaquindio.model.Producto;
+import co.edu.uniquindio.programacion3.subastaquindio.model.SubastaQuindio;
+import co.edu.uniquindio.programacion3.subastaquindio.utils.SubastaUtils;
+
+import java.util.List;
+
+
+public class ModelFactoryController implements IModelFactoryService {
+
+    SubastaQuindio subasta;
+    SubastaMapper mapper = SubastaMapper.INSTANCE;
+
+    //------------------------------  Singleton ------------------------------------------------
+    // Clase estatica oculta. Tan solo se instanciara el singleton una vez
+    private static class SingletonHolder {
+        private final static ModelFactoryController eINSTANCE = new ModelFactoryController();
+    }
+
+    // Método para obtener la instancia de nuestra clase
+    public static ModelFactoryController getInstance() {
+        return SingletonHolder.eINSTANCE;
+    }
+
+    public ModelFactoryController() {
+        System.out.println("invocación clase singleton");
+        cargarDatosBase();
+    }
+
+    private void cargarDatosBase() {
+        subasta = SubastaUtils.inicializarDatos();
+    }
+
+    public SubastaQuindio getSubasta() {
+        return subasta;
+    }
+
+    public void setSubasta(SubastaQuindio subasta) {
+        this.subasta = subasta;
+    }
+
+
+    @Override
+    public List<ProductoDTO> obtenerProductos() {
+        return  mapper.getProductoDto(subasta.getListaProductos());
+    }
+
+    @Override
+    public boolean agregarProducto(ProductoDTO productoDto) {
+        try{
+            if(!subasta.verificarProductoExistente(productoDto.codigoUnico())) {
+                Producto producto = mapper.productoDtoToProducto(productoDto);
+                getSubasta().agregarProducto(producto);
+            }
+            return true;
+        }catch (ProductoException e){
+            e.getMessage();
+            return false;
+        }
+    }
 }
